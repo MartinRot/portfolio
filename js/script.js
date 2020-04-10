eb56d4d4ccc756a9274ed361b01442a6a155349e
Martin <50304579+MartinRot@users.noreply.github.com>
2020-04-10 01:14:30 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function($){
+    var typed = new Typed('span.txt-rotate', {
+        strings: ["I'm a Web Developer Student", "I'm From Buenos Aires - Argentina"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        fadeOut: false,
+        smartBackspace: true,
+        loop: true,
+    });
+})(jQuery);
